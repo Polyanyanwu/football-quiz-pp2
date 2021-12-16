4aaac3a07bb317898b0c37c6abc3f813e7f44c99
Polycarp Anyanwu <polyanyanwu@gmail.com>
2021-12-16 21:11:16 +0000

    Modify style of answer total to grid, clean up and test html, css and javascript files

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,17 +2,17 @@
 "use strict";
 // The Config.js contains all static variables required for smooth functioning of the site. 
 // It also contains the detailed instruction to be displayed when the user desires to do so
-export const QUIZ_TIMEOUT_SEC = 480;
+export const QUIZ_TIMEOUT_SEC = 600;
 export const TOTAL_QUESTIONS_PER_SESSION = 10;
 export const PROFESSIONAL_MARK_PER_QUESTION = 10;
-export const AMATEUR_MARK_PER_QUESTION = 6.5;
+export const AMATEUR_MARK_PER_QUESTION = 7.5;
 export const MIN_USER_LENGTH = 3;
 export const TOTAL_ANSWER_OPTIONS = 4;
 export const PASS_CUTOFF_MARK = 60;
 export const MAX_AMATEUR_QUIZ = 8;  //If user answers 8 amateur questions force user to professional
 export const PLAY_INSTRUCTION = `<p>The Quiz is composed of two levels, the Amateur and the Professional. 
-The difference between the two is that the professional has more difficult to guess questions. Each Amateur question is 6.5 marks
-while each professional question is 10 marks. To pass the quiz you must answer at least 2 professional question correctly.
+The difference between the two is that the professional has more difficult to guess questions. Each Amateur question is 7.5 marks
+while each professional question is 10 marks. To pass the quiz you must answer at least 1 professional question correctly or answer all 8 amateur questions correctly.
 Click on the desired level of play and a question will be loaded automatically.<br><br>
 Click on your answer choice and the Submit Answer button to get the Guru to mark your submission.<br></p><p>
 If your answer is correct, the total correct answer is incresed by 1 and if wrong, the wrong answers is increased by 1.
@@ -22,4 +22,4 @@ The quix is timed. The remaining time is displayed to give you an idea of how yo
 and a percentage score is displayed. A sound is made at each correct or wrong answer submitted. If you attain the pass mark, a pleasing clap soud will be played. 
 </p><br>You are permitted to move to next question without attempting the current question if you desire, however 
 the Guru will request a confirmation that you wish to proceed to the next question. <p>If you answer 8 Amateur questions, the Guru will switch you over to Professional level.
-</p><p>You may click on the football image to see it animated.</p>` ; 
+</p><p><br>You may click on the football image to see it animated.</p>` ; 
